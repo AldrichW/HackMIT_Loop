fc85617f23f4efa78d5fe25e933690374f05a291
Aldrich Wingsiong <aldrich.wingsiong@gmail.com>
2013-10-05 21:00:48 -0400

    Implemented splash screen and sign up screen

--- Loop/gen/com/hackmit/loop/R.java
+++ Loop/gen/com/hackmit/loop/R.java
@@ -25,7 +25,10 @@ public final class R {
         public static final int logo_blue=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080003;
+        public static final int confirm_pass_field=0x7f080002;
+        public static final int email_field=0x7f080000;
+        public static final int pass_field=0x7f080001;
     }
     public static final class layout {
         public static final int activity_login=0x7f030000;
@@ -39,6 +42,10 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int placeholder_confirm_pass=0x7f050006;
+        public static final int placeholder_email=0x7f050004;
+        public static final int placeholder_pass=0x7f050005;
+        public static final int signup=0x7f050007;
     }
     public static final class style {
         /** 
